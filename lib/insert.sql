cb17b3915d07a90083917e72b4d4947d6487ea6a
Ndalila <vinson.steve@student.moringaschool.com>
2022-08-28 01:07:46 +0300

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex) VALUES ("Mr. Chocolate", "M");
+INSERT INTO bears (name, sex) VALUES ("Rowdy", "M");
+INSERT INTO bears (name, sex) VALUES ("Tabitha", "F");
+INSERT INTO bears (name, sex) VALUES ("Sergeant Brown", "F");
+INSERT INTO bears (name, sex) VALUES ("Melissa", "M");
+INSERT INTO bears (name, sex) VALUES ("Grinch", "F");
+INSERT INTO bears (name, sex) VALUES ("Wendy", "M");
+INSERT INTO bears (name, sex) VALUES (NULL, "F");
